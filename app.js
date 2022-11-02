9bfced1818e6860939894e7ec8778bbd1d0027bd
fransan990 <fransan990@gmail.com>
2022-11-02 12:25:11 +0100

    cors good npm

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ require("./db");
 const express = require("express");
 
 const app = express();
+var cors = require('cors')
 
 require("./config")(app);
-
-const allRoutes = require("./routes/index.routes");
-app.use("/api", allRoutes);
-
-require("./error-handling")(app);
 
 app.use(
     cors({
@@ -19,5 +15,10 @@ app.use(
         credentials: true
     })
 );
+
+const allRoutes = require("./routes/index.routes");
+app.use("/api", allRoutes);
+
+require("./error-handling")(app);
 
 module.exports = app;
